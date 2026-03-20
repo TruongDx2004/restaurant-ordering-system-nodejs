990ea421e956f71e660aa19f1705a1925a44dfcc
TruongDx2004 <doanxuantruong2110@gmail.com>
2026-03-20 17:09:40 +0700

    add: cors config

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const corsConfig = require("./config/cors");
+
 var indexRouter = require('./routes/index');
 var authRouter = require("./routes/authRoute");
 var userRouter = require("./routes/userRoute");
@@ -14,6 +16,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(corsConfig);
 
 app.use(logger('dev'));
 app.use(express.json());
