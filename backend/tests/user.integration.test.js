216b2198cdc0f07b3bbb904dad626e99ae21b150
TruongDx2004 <doanxuantruong2110@gmail.com>
2026-03-21 23:48:57 +0700

    test: UserAPI

--- backend/tests/user.integration.test.js
+++ backend/tests/user.integration.test.js
@@ -12,18 +12,4 @@ describe("Integration Test - User API", () => {
     const res = await request(app).get("/api/users");
     expect(res.statusCode).toBe(200);
   });
-
-  test("POST /api/users → 201", async () => {
-    const res = await request(app)
-      .post("/api/users")
-      .send({
-        name: "Test User",
-        email: "test@gmail.com",
-        password: "123456",
-        role: "EMPLOYEE"
-      });
-
-    expect([200, 201]).toContain(res.statusCode);
-  });
-
 });
